2152ef30d73f08e6f937455f28b2cac73810fff8
KIMGEUNDU <vlkoklv@naver.com>
2023-08-02 10:08:28 +0900

    docs: 파일 오류 수정 및 코드 수정

--- client/js/common/certifyPage.js
+++ client/js/common/certifyPage.js
@@ -17,3 +17,12 @@ setInterval(() => {
 	let time = new Date().toLocaleTimeString('ko-KR');
 	headerTime.textContent = time.slice(3, -3);
 }, 60000);
+
+//작성할 리뷰 없으면 텍스트 바꾸기
+const write__able = getNode('.write__able');
+if (reviewData.length === 0) {
+	write__able.textContent = '작성 가능한 리뷰가 없습니다 : )';
+	write__able.classList.add('writeAbleEmpty');
+} else {
+	write__able.classList.add('writeAble');
+}
