f916c61124888df962a12934ae9276743b8c1b48
o0orang <esksk7942@naver.com>
2023-08-02 10:59:43 +0900

    docs: logout.js, themePage.html 코드 수정

--- client/js/common/logout.js
+++ client/js/common/logout.js
@@ -5,12 +5,13 @@ const logoutBtn = getNode('.coupon');
 //console.log(idInput.value);
 async function renderLogout() {
   try {
-     const response = await userDataReview.get('http://localhost:3000/login/goo123');
+    console.log('완료');
+     const response = await userDataReview.get('http://localhost:3000/login');
      //const response = await userDataReview.get(`http://localhost:3000/login/${idInput.value}`);
       const userData = response.data;
-      console.log(userData.uniqueId);
+      //console.log(userData.uniqueId);
       localStorage.removeItem('uniqueId');
       location.href = 'login.html';
-  } catch {}
+  } catch { console.log('버튼완료');}
 }
 logoutBtn.addEventListener('click', renderLogout);
